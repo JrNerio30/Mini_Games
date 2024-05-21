c9509817a8ed6ea102ba0b3489605cfef148bb8e
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-05-20 20:45:28 -0600

    refactor: change file name from main.js to rock_paper_scissors.js

---
+++ src/rock_paper_scissors.js
@@ -0,0 +1,36 @@
+// VARIABLES
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const results = document.getElementById("result");
+
+// Event Listeners for the buttons
+rockButton.addEventListener("click", () => handleButtonClick("rock"));
+paperButton.addEventListener("click", () => handleButtonClick("paper"));
+scissorsButton.addEventListener("click", () => handleButtonClick("scissors"));
+
+function handleButtonClick(userChoice) {
+  const computerChoice = getComputerChoice();
+  const result = determineWinner(userChoice, computerChoice);
+  results.textContent = `You Chose ${userChoice}, computer chose ${computerChoice}. ${result}`;
+}
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return `IT'S A TIE!`;
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "WOW! YOU WIN";
+  } else {
+    return "HAHA, YOU LOSE";
+  }
+}
